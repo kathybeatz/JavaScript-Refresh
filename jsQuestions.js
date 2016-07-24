1e5e7f5a61ca39df4edb159b451cbb5da75d095c
Kathleen L <kathleeng.lopez@gmail.com>
2016-07-24 16:54:10 -0700

    Check if a number is an integer, output of code, write a function that will allow you to do this are added

---
+++ jsQuestions.js
@@ -0,0 +1,50 @@
+//1) How would you check if a number is an integer?
+// To check if a number is a decimal or an integer, check to see if there is a remainder left when you divide by 1
+
+function isInt(num) {
+  return num % 1 === 0;
+};
+
+console.log(isInt(4)); //true
+console.log(isInt(4.4)); //false
+console.log(isInt(0.2)); //false
+
+//-----------------------------
+
+// //2) What will the following code output?
+(function() {
+  var a = b = 5;
+})();
+
+console.log(b); //5
+
+/*
+
+It looks like a and b are within the scope of the anonymous function. Variables declared within a function are not accessible outside of it.
+
+The output will be 5 because var a = b = 5; is interpreted as:
+  var a = b;
+  b = 5;
+
+b is in the global scope because it is not declared within the anonymous function with a var.
+
+*/
+
+//-----------------------------
+
+//3) Write a function that would allow you to do this:
+
+/*
+  You can crate a closure to keep the value of 'a' even after the inner function is return. 
+  The inner function that is being returned is created within an outer function, making it a 'closure', and it has access to the variables within the outer function, in this case variable 'a'.
+*/
+
+function multiply(a) {
+  return function(b) {
+    return a * b;
+  };
+};
+
+console.log(multiply(5)(6)); 
+
+//-----------------------------
