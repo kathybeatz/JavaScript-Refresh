50fc32d8fd8b7d4a76b440473642363cb8e02a1c
Kathleen L <kathleeng.lopez@gmail.com>
2016-07-24 18:29:16 -0700

    Added what wil lthe following code output - floating decimals

--- jsQuestions.js
+++ jsQuestions.js
@@ -164,6 +164,17 @@ modifyArray(arr, function() {
   console.log("array has been modified", arr);
 });
 
+//-----------------------------
+
+//9) What will the following code output?
+
+0.1 + 0.2 === 3; //false
+
+//This will return false because of 'floating point' errors in internally representing certain numbers. 0.1 + 0.2 does not nicely come out to 0.3 because the result is actually 0.30000000000000004 because the computer cannot internally represent the correct number.
+//One solution to get around this problem is to round the results when doing arithmetic with decimals.
+
+
+
 
 
 
