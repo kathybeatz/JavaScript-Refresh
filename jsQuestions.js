f607c288af8b151c80dc1e39bbd5aa0f08898fc0
Kathleen L <kathleeng.lopez@gmail.com>
2016-07-24 17:01:35 -0700

    Added what does "use strict" do

--- jsQuestions.js
+++ jsQuestions.js
@@ -71,3 +71,22 @@ console.log(fullName.bind(person)()); // Hello this is Foo Bar
 
 
 
+// 5) What does "use strict" do?
+
+/*
+  The "use strict" literal is entered at the top of a JavaScript program or at the top of a function and it helps you write safer JavaScript code by throwing an error if a global variable is created by mistake. 
+*/
+
+//For example, the following program will throw an error:
+function doSomething(val) {
+  "use strict";
+  x = val + 10;
+};
+
+//It will throw an error becasue x was not defined and it is being set to some value in the global scope, which isnt allowed with "use strict" 
+function doSomething(val) {
+  "use strict";
+  var x = val + 10; // adding var will fix the error
+};
+
+
