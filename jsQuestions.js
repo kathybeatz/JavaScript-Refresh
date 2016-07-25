ab59d4d3da756193eb4c8b30063eae8877cbef5d
Kathleen L <kathleeng.lopez@gmail.com>
2016-07-24 18:25:10 -0700

    Added what is a callback and provide and example

--- jsQuestions.js
+++ jsQuestions.js
@@ -144,6 +144,26 @@ console.log(avg); //3
 
 //Now every array that is created wil have the average method inherited along with every other Array method. But, you need to be careful with modifying the prototype of an object, because you can mistakenly overwrite a common method.
 
+//-----------------------------
+
+// 8) Explain what a callback function is and provide a simple example.
+
+//A callback function is a function that is passed to another function as an argument and is executed after some operation has been completed. 
+//Below is an example of a simple callback function that logs to the console 'after' some operations have been completed.
+
+function modifyArray(arr, callback) {
+  //do something to arr here
+  arr.push(100);
+  //then execute the callback function that was passed
+  callback();
+};
+
+var arr = [1, 2, 3, 4, 5];
+
+modifyArray(arr, function() {
+  console.log("array has been modified", arr);
+});
+
 
 
 
